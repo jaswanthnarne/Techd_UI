e43dc698e6f6729dde02e6833974d004bdebc33d
jaswanthnarne <2203031260150@paruluniversity.ac.in>
2025-10-19 00:42:23 +0530

    commmit for the backend intergration in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL="https://techddb.vercel.app/api";
 
 // Create axios instance
 const api = axios.create({
